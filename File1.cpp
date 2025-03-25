a7cbad664703d1fcd760dc3b6e00a1a796e00200
spicyshibe <rafierasyw@gmail.com>
2025-03-25 18:55:32 +0700

    step 4

--- File1.cpp
+++ File1.cpp
@@ -6,5 +6,45 @@ int n;
 
 void input()
 {
-    
+    while (true)
+    {
+        cout << "masukkan jumlah data pada array :";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else 
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "==================" << endl;
+    cout << "masukkan elemen array" << endl;
+    cout << "==================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insertionSort()
+{
+    int temp;
+    int j, i;
+
+    for (i = 1; i <= n - 1; i++)
+    {
+        temp = arr[i];
+        j = i -1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+
+        }
+    }
 }
