5fc6ec39be0780925d228b447ad72592f81ec508
spicyshibe <rafierasyw@gmail.com>
2025-03-25 19:04:32 +0700

    void display dan int main

--- File1.cpp
+++ File1.cpp
@@ -60,4 +60,16 @@ void display()
     cout << "===========================" << endl;
     cout << "elemen array yang telah tersusun" << endl;
     cout << "===========================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+
+    }
+    cout << endl;
+}
+
+int main()
+{
+    
 }
